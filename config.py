43cc5aee5b43dd196718b52b36d4a7be14810889
Agaichatou Nouhou <agaichatounouhou@gmail.com>
2026-05-14 17:12:55 +0000

    Fix icône mot de passe

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-a-changer-en-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///auth_system.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = 'jarring-operator-heaviness.ngrok-free.dev'
     PREFERRED_URL_SCHEME = 'https'
